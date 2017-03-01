0d48817524b039e9d0f594203460039df0f20c35
spmcenaney <spmcenaney@gmail.com>
2017-03-01 12:41:41 -0600

    state

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VelocityVortex/StateVelocityVortexAutoRedRamp.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.VelocityVortex;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+/**
+ * Created by spmce on 2/25/2017.
+ */
+@Autonomous(name = "StateFullAutoRedRamp", group = "StateAutonomous")
+public class StateVelocityVortexAutoRedRamp extends VVFullAuto {
+    /**
+     * constructor
+     */
+    public StateVelocityVortexAutoRedRamp() {
+        // constructor
+    }
+
+    /**
+     * Init
+     */
+    //@Override
+    public void init() {
+        super.init();
+    }
+
+    /**
+     * Loop
+     */
+    //@Override
+    public void loop() {
+        autoLoop(false,true);
+    }
+}
